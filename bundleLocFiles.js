be168f56412c42e0c1f90c88dcc6a0f6f0ee2a8c
Dan Silver <dansil@microsoft.com>
2017-01-13 10:27:33 -0800

    history button background fix

---
+++ bundleLocFiles.js
@@ -0,0 +1,37 @@
+// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+
+// Bundles files in translation_files/ to scripts/loc_strings.js
+
+
+var fs = require('fs');
+
+fs.readdir("translation_files", function(err, filenames) {
+    if (err) {
+        onError(err);
+        return;
+    }
+    var fileStr = '// This is a generated file from bundleLocFiles.js \n\nvar loc_strings = {};'
+    var languageRead = [];
+    filenames.forEach(function(filename) {
+        languageRead.push(new Promise(function(resolve) {
+            fs.readFile("translation_files/" + filename, 'utf-8', function(err, content) {
+                fileStr += '\n\n' + 'loc_strings[\'' + filename.split(".")[0] + '\'] = ' + JSON.stringify(JSON.parse(content));
+                resolve();
+            });
+        }))
+    });
+    Promise.all(languageRead).then(() => {
+        fs.writeFile("scripts/loc_strings.js", fileStr, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+
+            console.log("The file was saved!");
+        }); 
+    })
+});
+
+
