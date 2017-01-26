514c41cba4a17936436ea1c7fe5b810898e3ad0e
Dan Silver <dansil@microsoft.com>
2017-01-26 14:43:40 -0800

    Fixed extra slash when clicking links in json response

---
+++ scripts/api-explorer-msgraph.d.ts
@@ -0,0 +1,2 @@
+declare function endsWithSlash(serviceText: string): boolean;
+declare function msGraphLinkResolution($scope: any, body: any, args: any, service: any): void;
--- scripts/api-explorer-msgraph.ts
+++ scripts/api-explorer-msgraph.ts
@@ -1,15 +1,21 @@
-function endsWithSlash(serviceText) {
-    return serviceText.charAt(serviceText.length - 1) == '/';
+// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+function endsWithSlash(serviceText:string):boolean {
+    return serviceText.charAt(serviceText.length-1) == '/';
 }
-function msGraphLinkResolution($scope, body, args, service) {
+
+function msGraphLinkResolution ($scope, body, args, service) {
     if (args.indexOf("https://") == -1) {
         debugger;
         if (service.text.indexOf(args.substr(1)) != -1) {
-        }
-        else if (service.text.indexOf("/me") != -1 && service.text.indexOf("/memberOf") == -1) {
+
+        } else if (service.text.indexOf("/me") != -1 && service.text.indexOf("/memberOf") == -1) {
             service.text = service.text.replace("/me", "") + "/users/" + args.substr(1);
-        }
-        else {
+        } else {
+
+            // if type exists
             var index = body.indexOf(args.substr(1));
             var typeIndex = body.lastIndexOf('@odata.type', index);
             if (typeIndex != -1) {
@@ -18,25 +24,24 @@ function msGraphLinkResolution($scope, body, args, service) {
                 type = type.replace("@odata.type\": \"#microsoft.graph.", "");
                 type = type.replace("\"", "").replace(",", "");
                 service.text = "https://graph.microsoft.com/v1.0/" + type + "s/" + args.substr(1);
-            }
-            else {
+            } else {
                 if (service.text.indexOf("?") != -1) {
                     service.text = service.text.substr(0, service.text.indexOf("?"));
                 }
-                var linkUrl = [service.text, args.substr(1)];
+
+                const linkUrl = [service.text, args.substr(1)];
                 if (endsWithSlash(service.text))
                     service.text = linkUrl.join("");
                 else
                     service.text = linkUrl.join("/");
             }
         }
-    }
-    else {
+    } else {
         service.text = args.replace("\"", "");
     }
+
     if (service.text && endsWithSlash(service.text)) {
-        service.text += '/';
+                service.text += '/';
     }
     $scope.submit(service.text);
 }
-//# sourceMappingURL=api-explorer-msgraph.js.map
