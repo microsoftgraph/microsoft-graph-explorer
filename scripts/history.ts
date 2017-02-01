c1fe5ec15b42190ab545e21ce0cdd0b9c18d6572
Dan Silver <dansil@microsoft.com>
2017-02-01 14:52:39 -0800

    Strongly typed history operations

--- scripts/history.ts
+++ scripts/history.ts
@@ -1,22 +1,39 @@
-var requestHistory = [];
-var LocalStorageKeyGraphRequestHistory = "GRAPH_REQUEST_HISTORY";
+let requestHistory:HistoryRecord[] = [];
+
+interface HistoryRecord {
+    successful: boolean,
+    statusCode: number,
+    duration: number,
+    requestId: string
+}
+
+const LocalStorageKeyGraphRequestHistory = "GRAPH_REQUEST_HISTORY";
+
 function saveHistoryToLocalStorage() {
     localStorage.setItem(LocalStorageKeyGraphRequestHistory, JSON.stringify(requestHistory));
 }
+
 function loadHistoryFromLocalStorage() {
-    var possibleHistory = localStorage.getItem(LocalStorageKeyGraphRequestHistory);
+    let possibleHistory = localStorage.getItem(LocalStorageKeyGraphRequestHistory);
+
     if (possibleHistory == null) {
         return;
     }
+
     requestHistory = JSON.parse(possibleHistory);
 }
-function saveHistoryObject(historyObject, statusCode, duration) {
+
+function saveHistoryObject(historyObject:HistoryRecord, statusCode: number, duration:number) {
     historyObject.successful = statusCode >= 200 && statusCode < 300;
     historyObject.statusCode = statusCode;
     historyObject.duration = duration;
     historyObject.requestId = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
-    requestHistory.splice(0, 0, historyObject);
+
+    requestHistory.splice(0, 0, historyObject); //add history object to the array
+
     saveHistoryToLocalStorage();
 }
+
+
+// init scripts
 loadHistoryFromLocalStorage();
-//# sourceMappingURL=history.js.map
