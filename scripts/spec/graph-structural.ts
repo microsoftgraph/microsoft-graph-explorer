e79622cd4244f4a3a4e86eacaefc2fc9858a8c39
Dan Silver <dansil@microsoft.com>
2017-03-02 15:54:53 -0800

    Use assert.equal for better error messages

--- scripts/spec/graph-structural.ts
+++ scripts/spec/graph-structural.ts
@@ -36,50 +36,62 @@ describe("Graph Structural Tests", function() {
     describe("Path parsing from metadata graph", function() {
       it("https://graph.microsoft.com/v1.0/me => [user]", function() {
         return constructGraphLinksFromFullPath("https://graph.microsoft.com/v1.0/me").then((links) => {
-          chai.assert(links.length == 1)
-          chai.assert(links[0].type == "microsoft.graph.user")
+          chai.assert.equal(links.length, 1)
+          chai.assert.equal(links[0].type, "microsoft.graph.user")
         })
       });
 
       it("https://graph.microsoft.com/v1.0/users/foobar@contoso.com/calendar => [users] -> [user] -> [calendar]", function() {
         return constructGraphLinksFromFullPath("https://graph.microsoft.com/v1.0/users/foobar@contoso.com/calendar").then((links) => {
-          chai.assert(links.length == 3)
+          chai.assert.equal(links.length, 3)
 
-          // users collection
-          chai.assert(links[0].type == "microsoft.graph.user")
-          chai.assert(links[0].isACollection == true)
+          chai.assert.equal(links[0].type, "microsoft.graph.user")
+          chai.assert.equal(links[0].isACollection, true)
 
-          // single user entity
-          chai.assert(links[1].type == "microsoft.graph.user")
-          chai.assert(links[1].isACollection == false)
-          chai.assert(links[1].name == "foobar@contoso.com")
+          chai.assert.equal(links[1].type, "microsoft.graph.user")
+          chai.assert.equal(links[1].isACollection, false)
+          chai.assert.equal(links[1].name, "foobar@contoso.com")
 
-          // calendar
-          chai.assert(links[2].type == "microsoft.graph.calendar")
-          chai.assert(links[2].isACollection == false)
+          chai.assert.equal(links[2].type, "microsoft.graph.calendar")
+          chai.assert.equal(links[2].isACollection, false)
 
         });
       });
 
       it("https://graph.microsoft.com/beta/me/photos/x/width => [user] -> [profilePhoto collection] -> [profilePhoto] -> [width property]", function() {
         return constructGraphLinksFromFullPath("https://graph.microsoft.com/beta/me/photos/x/width").then((links) => {
-          chai.assert(links.length == 4)
+          chai.assert.equal(links.length, 4)
 
-          chai.assert(links[0].type == "microsoft.graph.user")
-          chai.assert(links[0].isACollection == false)
+          chai.assert.equal(links[0].type, "microsoft.graph.user")
+          chai.assert.equal(links[0].isACollection, false)
 
-          chai.assert(links[1].type == "microsoft.graph.profilePhoto")
-          chai.assert(links[1].isACollection == true)
+          chai.assert.equal(links[1].type, "microsoft.graph.profilePhoto")
+          chai.assert.equal(links[1].isACollection, true)
 
           
-          chai.assert(links[2].type == "microsoft.graph.profilePhoto")
-          chai.assert(links[2].isACollection == false)
-          chai.assert(links[2].name == "x")
+          chai.assert.equal(links[2].type, "microsoft.graph.profilePhoto")
+          chai.assert.equal(links[2].isACollection, false)
+          chai.assert.equal(links[2].name, "x")
 
-          chai.assert(links[3].name == "width")
-          chai.assert(links[3].type == "Edm.Int32")
-          chai.assert(links[3].tagName == "Property")
+          chai.assert.equal(links[3].name, "width")
+          chai.assert.equal(links[3].type, "Edm.Int32")
+          chai.assert.equal(links[3].tagName, "Property")
 
+        });
+      });
+
+      it("https://graph.microsoft.com/beta/me/city => [microsoft.graph.user] -> [city property]", function() {
+        return constructGraphLinksFromFullPath("https://graph.microsoft.com/beta/me/city").then((links) => {
+          chai.assert.equal(links.length, 2)
+
+          chai.assert.equal(links[0].type, "microsoft.graph.user")
+          chai.assert.equal(links[0].isACollection, false)
+          chai.assert.equal(links[0].tagName, "Singleton")
+
+          chai.assert.equal(links[1].tagName, "Property")
+          chai.assert.equal(links[1].isACollection, false)
+          chai.assert.equal(links[1].name, "city")
+          chai.assert.equal(links[1].type, "Edm.String")
         });
       });
     });
@@ -107,32 +119,32 @@ describe("Graph Structural Tests", function() {
 
       it ('A POST to /v2.5/me is saved', function() {
           //create an object to store the api call
-          let historyObj:HistoryRecord = {};
+          const historyObj:HistoryRecord = {};
           historyObj.urlText = "https://graph.microsoft.com/v1.0/me/";
           historyObj.selectedVersion = "v2.5";
           historyObj.htmlOption = "POST";
           saveHistoryObject(historyObj, 400);
 
-          let savedHistoryObj = fetchRequestHistory().pop();
-          chai.assert(savedHistoryObj.htmlOption == historyObj.htmlOption)
-          chai.assert(savedHistoryObj.selectedVersion == historyObj.selectedVersion)
-          chai.assert(savedHistoryObj.urlText == historyObj.urlText)
-          chai.assert(savedHistoryObj.duration == historyObj.duration)
+          const savedHistoryObj = fetchRequestHistory().pop();
+          chai.assert.equal(savedHistoryObj.htmlOption, historyObj.htmlOption)
+          chai.assert.equal(savedHistoryObj.selectedVersion, historyObj.selectedVersion)
+          chai.assert.equal(savedHistoryObj.urlText, historyObj.urlText)
+          chai.assert.equal(savedHistoryObj.duration, historyObj.duration)
       });
 
       it ('A GET to /beta/me is saved', function() {
           //create an object to store the api call
-          let historyObj:HistoryRecord = {};
+          const historyObj:HistoryRecord = {};
           historyObj.urlText = "https://graph.microsoft.com/beta/me/";
           historyObj.selectedVersion = "v2.5";
           historyObj.htmlOption = "GET";
           saveHistoryObject(historyObj, 200);
 
-          var savedHistoryObj = fetchRequestHistory().pop();
-          chai.assert(savedHistoryObj.htmlOption == historyObj.htmlOption)
-          chai.assert(savedHistoryObj.selectedVersion == historyObj.selectedVersion)
-          chai.assert(savedHistoryObj.urlText == historyObj.urlText)
-          chai.assert(savedHistoryObj.duration == historyObj.duration)
+          const savedHistoryObj = fetchRequestHistory().pop();
+          chai.assert.equal(savedHistoryObj.htmlOption, historyObj.htmlOption)
+          chai.assert.equal(savedHistoryObj.selectedVersion, historyObj.selectedVersion)
+          chai.assert.equal(savedHistoryObj.urlText, historyObj.urlText)
+          chai.assert.equal(savedHistoryObj.duration, historyObj.duration)
       });
   });
 });
