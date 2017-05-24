5ff9900d23bdbc9a25b56d8ab687a6c2f718f17b
Dan Silver <dannysilver3@gmail.com>
2017-05-24 16:42:00 -0700

    Removed Teams queries

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var platform_browser_1 = require("@angular/platform-browser");
-var app_module_ngfactory_1 = require("./aot/app/app.module.ngfactory");
-var core_1 = require("@angular/core");
-core_1.enableProdMode();
-platform_browser_1.platformBrowser().bootstrapModuleFactory(app_module_ngfactory_1.AppModuleNgFactory);
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModuleNgFactory } from './aot/app/app.module.ngfactory';
+import { enableProdMode } from "@angular/core";
+enableProdMode();
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 //# sourceMappingURL=main.js.map
