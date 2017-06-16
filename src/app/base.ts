40917a87671cc00760159a932d818e3236eb64fc
Dan Silver <dansil@microsoft.com>
2017-06-16 14:56:19 -0700

    Code comments for tokens

--- src/app/base.ts
+++ src/app/base.ts
@@ -142,6 +142,19 @@ export const CommonHeaders = [
     "Warning"
 ];
 
+/**
+ * Tokens are used as placeholder values in sample queries to cover many scenarios:
+ * - ID tokens for sample tenant nodes like user IDs, file IDs and other string constants
+ * - Tokens that must be determined at runtime like the current date
+ * - Tokens that are determined from the authenticated users session
+ * - Tokens can be in the POST body or part of the URL
+ *
+ * The token fields are split into default, demo and authenticated. If neither the demo or
+ * auth values are supplied, the token falls back to the default value.
+ *
+ * Tokens are maintained in tokens.ts.
+ */
+
 export interface Token {
     placeholder: string
 
@@ -190,6 +203,11 @@ function getTokenSubstituteValue(token:Token) {
 
 }
 
+/**
+ * Given a query, replace all tokens in the request URL and the POST body with thier
+ * values.  When a token is found, use getTokenSubstituteValue() to find the right
+ * value to substitute based on the session.
+ */
 export function substituteTokens(query:SampleQuery) {
     type QueryFields = keyof GraphApiCall;
 
