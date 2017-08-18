2969ed826788db510fc66fb720df20b15afc3c45
Dan Silver <dansil@microsoft.com>
2017-08-18 14:12:52 -0700

    Generated prod build

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { platformBrowser } from '@angular/platform-browser';
+import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from './aot/app/app.module.ngfactory';
-import { enableProdMode } from "@angular/core";
 enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 //# sourceMappingURL=main.js.map
