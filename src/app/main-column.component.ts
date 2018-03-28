622b6f9fbfba24fb12672a0d40270c61adaea500
Michael <mmainer@microsoft.com>
2018-03-28 10:28:44 -0700

    Removed AfterViewChecked

--- src/app/main-column.component.ts
+++ src/app/main-column.component.ts
@@ -2,7 +2,7 @@
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------
 
-import { Component, AfterViewInit, ViewChild, ViewContainerRef, DoCheck, AfterViewChecked } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ViewContainerRef, DoCheck } from '@angular/core';
 import { Methods, ExplorerValues, MessageBarContent, GraphApiVersion } from "./base";
 import { GraphExplorerComponent } from "./GraphExplorerComponent";
 import { AppComponent } from "./app.component";
