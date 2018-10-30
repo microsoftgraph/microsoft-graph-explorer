f87b82cd9d31c6175f2913a30908c84ec7ff27ae
Japheth Obala <japhethobalak@gmail.com>
2018-10-30 09:03:38 +0300

    Adds core-js/es7/reflect to tests.ts

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,7 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
 import 'zone.js/dist/zone-testing';
+import 'core-js/es7/reflect';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
