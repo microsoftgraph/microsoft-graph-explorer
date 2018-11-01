4f732e7f3b926d42fa31ca8e3284f4fc3a5484f3
Charles Wahome <thewahome@users.noreply.github.com>
2018-11-01 16:20:49 +0300

    import jwt-decode

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -27,8 +27,9 @@
       '@angular/platform-browser/animations': 'npm:@angular/platform-browser/bundles/platform-browser-animations.umd.js',
 
       // other libraries
-      'rxjs':                      'npm:rxjs',
+      'rxjs': 'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
+      'jwt-decode': 'npm:jwt-decode/build/jwt-decode.min.js',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
