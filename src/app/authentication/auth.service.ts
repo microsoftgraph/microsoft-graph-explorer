960f6c4810fe5848142e70c3f303284d900546f2
Charles Wahome <thewahome@users.noreply.github.com>
2018-11-06 23:05:33 +0300

    create callback for acquireTokenRedirect

--- src/app/authentication/auth.service.ts
+++ src/app/authentication/auth.service.ts
@@ -6,22 +6,30 @@ declare let Msal: any;
 
 @Injectable()
 export class AuthService {
+
     private app: any;
 
     constructor() {
-        this.app = new Msal.UserAgentApplication((window as any).ClientId, '', this.authCallback, {});
+        this.app = new Msal.UserAgentApplication((window as any).ClientId, '', this.authCallback,
+            this.acquireTokenRedirectCallBack, {});
     }
 
-    public authCallback = async (errorDesc, token, error) => {
+    public authCallback = (errorDesc, token, error) => {
         if (token) {
             localStorage.setItem('status', 'authenticated');
-            await this.getToken();
+            this.getToken();
         } else if (errorDesc || error) {
             localStorage.setItem('status', 'anonymous');
         }
     }
 
-    public async login() {
+    public acquireTokenRedirectCallBack = (errorDesc, token, error, tokenType) => {
+        if (tokenType === 'access_token') {
+            return token;
+        }
+    }
+
+    public login() {
         return this.app.loginRedirect(this.defaultUserScopes());
     }
 
@@ -39,12 +47,7 @@ export class AuthService {
             .then((accessToken) => {
                 return accessToken;
             }, (error) => {
-                return this.app.acquireTokenPopup(listOfScopes)
-                    .then((accessToken) => {
-                        return accessToken;
-                    }, (err) => {
-                        localStorage.setItem('status', 'anonymous');
-                    });
+                return this.app.acquireTokenRedirect(listOfScopes);
             });
     }
 
