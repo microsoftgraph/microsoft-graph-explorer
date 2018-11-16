2d2e60c33071e9047668f14bd8132076588e5e5a
Japheth Obala <japhethobalak@gmail.com>
2018-11-16 11:58:02 +0300

    Moves compiler-output to src/

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      'app': 'app',
+      'app': 'compiler-output/app',
 
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -30,6 +30,7 @@
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
     },
+
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
