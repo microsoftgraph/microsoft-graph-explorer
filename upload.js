38fa413eae69fcb0eda5b37059c2b8617e2a5a18
Japheth Obala <japhethobalak@gmail.com>
2018-11-27 21:11:58 +0300

    Adds upload script

---
+++ upload.js
@@ -0,0 +1,51 @@
+const dotenv = require('dotenv');
+const fs = require('fs');
+const {
+    Aborter,
+    BlobURL,
+    BlockBlobURL,
+    ContainerURL,
+    ServiceURL,
+    StorageURL,
+    SharedKeyCredential,
+} = require("@azure/storage-blob");
+
+dotenv.load();
+
+
+const account = process.env.AZURE_STORAGE_ACCOUNT;
+const accountKey = process.env.AZURE_STORAGE_ACCESS_KEY;
+
+const sharedCredential = new SharedKeyCredential(account, accountKey);
+
+const pipeline = StorageURL.newPipeline(sharedCredential);
+const serviceURL = new ServiceURL(
+    // When using AnonymousCredential, following url should include a valid SAS or support public access
+    `https://${account}.blob.core.windows.net`,
+    pipeline
+);
+
+const containerURL = ContainerURL.fromServiceURL(serviceURL, 'staging/Scripts/build/js/GraphExplorer');
+
+const productionBuild = fs.readFileSync('dist/explorer.js');
+const blobName = 'explorer.js';
+
+const blobURL = BlobURL.fromContainerURL(containerURL, blobName);
+const blockBlobURL = BlockBlobURL.fromBlobURL(blobURL);
+
+async function upload() {
+    try {
+        const uploadBlobResponse = await blockBlobURL.upload(
+            Aborter.none,
+            productionBuild,
+            productionBuild.length,
+        );
+        console.log('Staged changes')
+
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+upload();
