a3a04799dec22ca34571aa34b2cb066282348075
Japheth Obala <japhethobalak@gmail.com>
2019-04-17 11:17:04 +0300

    Calls done.fail on 4xx errors

--- src/app/generate-queries/gen-queries.spec.ts
+++ src/app/generate-queries/gen-queries.spec.ts
@@ -121,7 +121,7 @@ describe('Sample query validation', () => {
           }
           done();
         }).catch((e: Response) => {
-          if (e.status !== 500) {
+          if (e.status < 500) {
             done.fail(`${query.humanName}: Can't execute sample GET request, ${e.status}, ${JSON.stringify(e.json())}`);
           }
           done();
