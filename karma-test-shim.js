0b0e525b77f43031c51c3b5366040aa5c9c89155
Japheth Obala <japhethobalak@gmail.com>
2019-07-01 10:40:08 +0300

    Increases test timeout

--- karma-test-shim.js
+++ karma-test-shim.js
@@ -4,7 +4,7 @@ Error.stackTraceLimit = 0; // "No stacktrace"" is usually best for app testing.
 // Uncomment to get full stacktrace output. Sometimes helpful, usually not.
 // Error.stackTraceLimit = Infinity; //
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000*15; // We are getting random timeouts at 10 sec, increasing to 15 seconds.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000*18; // We are getting random timeouts at 10 sec, increasing to 15 seconds.
 
 // builtPaths: root paths for output ("built") files
 // get from karma.config.js, then prefix with '/base/' (default is 'src/')
