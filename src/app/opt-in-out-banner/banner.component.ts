1db15fa918c2ef73990f631e6544876a730f6333
Charles Wahome <53429535+cwahome@users.noreply.github.com>
2019-10-15 10:27:26 +0300

    Feature: preview opt in (#368)
    * switch from classic to preview
    
    * localise banner text
    
    * change banner location
    
    * change banner messaging

---
+++ src/app/opt-in-out-banner/banner.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { GraphExplorerComponent } from '../GraphExplorerComponent';
+
+@Component({
+    selector: 'banner',
+    templateUrl: './banner.component.html',
+    styleUrls: ['./banner.component.css'],
+})
+export class BannerComponent extends GraphExplorerComponent {
+    public successClass = 'ms-Toggle-field';
+
+    constructor() {
+        super();
+    }
+
+    public switchToggle() {
+        this.successClass = 'ms-Toggle-field is-selected';
+        const path = location.href;
+        const urlObject: URL = new URL(path);
+        const { protocol, hostname, pathname, port } = urlObject;
+        const url = `${protocol}//${hostname}${(port) ? ':' + port : ''}${pathname}`;
+        window.location.href = url.includes('localhost') ? 'http://localhost:3001' : `${url}/preview`;
+    }
+
+}
