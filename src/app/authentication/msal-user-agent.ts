8cf64e57becdf500e60c9da45c7ee042040e5739
jobala <japhethobalak@gmail.com>
2020-01-29 15:39:25 -0800

    Turns off msal logging

--- src/app/authentication/msal-user-agent.ts
+++ src/app/authentication/msal-user-agent.ts
@@ -1,13 +1,8 @@
 import * as Msal from 'msal';
+import { telemetry } from '../telemetry';
 import { collectLogs } from './auth.service';
 
 const { ClientId } = (window as any);
-
-const loggerCallback = (level: Msal.LogLevel, message: string): void => {
-  collectLogs(message);
-};
-
-const logger = new Msal.Logger(loggerCallback, { level: Msal.LogLevel.Info });
 
 const config: Msal.Configuration = {
   auth: {
@@ -17,9 +12,6 @@ const config: Msal.Configuration = {
   cache: {
     cacheLocation: 'localStorage',
     storeAuthStateInCookie: true,
-  },
-  system: {
-    logger,
   },
 };
 
